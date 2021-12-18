4fbe49882e83c425d5a9fe6e9430a619418b675e
Timothy Otieno <timothyking094@gmail.com>
2021-12-18 07:30:39 +0300

    demonstrates basic selectors and filters in jQuery

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
+/** BASIC JQUERY SELECTORS */
 
+// $(
+//     function(){
+//         /** 1. Basic Jquery Selectors */
+//         // $('p').css('border', '3px solid red')
+//         // $('.a').css('border', '3px solid red')
+//         // $('#example').css('border', '3px solid red')
 
+//         /** 2. Basic Jquery Filters */
+//         // $('#example p:first').css('border', '3px solid red')
+//         // $('#example p:last').css('border', '3px solid red')
+//         // $('#example p:even').css('background-color', 'grey')
+//         // $('#example p:odd').css('background-color', 'navy')
+//         // $('#example p:not(p:odd)').css('border', '3px solid red')
+//         // $('#example p:lt(3)').css('border', '3px solid red')
+//     }
+// )
